bd349e85d6d189e5eae5553175a1d5c49b8e5d76
jorelds <jhoel.delta2009@gmail.com>
2026-08-12 19:26:03 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+string frase1 = $"Olá {nome}, hoje é {DateTime.Now}";
+Console.WriteLine(frase1);
+
+Console.WriteLine("===========================");
+
+Console.WriteLine("Quanto custa um dólar em reais?");
+decimal ValorDolarReais = decimal.Parse(Console.ReadLine());
+string frase2 = string.Format("Hoje é {0:dd/MM/yyyy}, o dólar está custando {1:c2}", DateTime.Now, ValorDolarReais);
+Console.WriteLine(frase2);
+
+Console.WriteLine("===========================");
+
+string cabecalho = string.Format("{0:dddd}, {0:dd} de {0:MMMM} de {0:yy} - {0:HH:mm:ss}", DateTime.Now);
+Console.WriteLine(cabecalho);
